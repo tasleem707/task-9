195b3944e3c93f09eaef3d9634af405f9500b59b
tasleem707 <testing95135@gmail.com>
2025-08-13 16:18:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "thematic-vertex-463206-a5"
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "credentials_file" {
+  description = "Path to the service account key file"
+  type        = string
+  default     = "credentials.json"
+}
 
