8c6eae5c4c35292b356060616bb65eca4525764c
tasleem707 <testing95135@gmail.com>
2025-08-14 15:08:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,13 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default     = "us-central1"
+  default     = "us-east1"
 }
 
 variable "zone" {
   description = "GCP Zone"
   type        = string
-  default     = "us-central1-a"
+  default     = "us-east1-b"
 }
 
 variable "credentials_file" {
